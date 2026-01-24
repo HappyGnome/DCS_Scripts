36da1c0fbf552128ca527b10961178c438882a48
HappyGnome <46838482+HappyGnome@users.noreply.github.com>
2026-01-24 17:29:03 +0000

    Added json serialization helper

--- HeLMS/helms.lua
+++ HeLMS/helms.lua
@@ -7,7 +7,7 @@
 --#######################################################################################################
 
 --NAMESPACES----------------------------------------------------------------------------------------------
-helms = { version = 1.16 }
+helms = { version = 1.17 }
 
 ----------------------------------------------------------------------------------------------------------
 --helms.util - LUA EXTENSIONS-----------------------------------------------------------------------------
@@ -66,6 +66,91 @@ helms.util.obj2str = function(obj)
         msg = msg .. t
     end
     return msg
+end
+--[[------------------------------------------
+		Escape reserved characters within 
+        a lua string
+--]]------------------------------------------
+helms.util.escapeLuaString = function (str) 
+	return 
+	string.gsub(string.gsub(string.gsub(string.gsub(str,"\\","\\\\")
+														,"\"","\\\"")
+														,"\n","\\n")
+														, "\r","\\r")
+end
+
+--[[------------------------------------------
+		Convert lua object to valid json
+--]]------------------------------------------
+helms.util.obj2json = function(obj, antiCirc,maxdepth)
+
+	if maxdepth == nil then 
+		maxdepth = 4 
+	end
+
+	if antiCirc == nil then 
+		antiCirc = {}
+	end
+
+	if maxdepth<=0 then
+		return ''
+	end
+
+	if obj == nil then 
+		return ''
+	end
+
+	local msg = ''
+	local t = type(obj)
+
+	if t == 'table' then
+		antiCirc[obj] = true
+		local first = true
+
+		if #obj > 0 then -- Array or object
+			msg = msg..'['
+			for k,v in ipairs(obj) do
+				local t = type(v)
+				if not antiCirc[v] then
+					if not first then 
+						msg = msg .. ","
+					end
+					msg = msg .. helms.util.obj2json(v,antiCirc,maxdepth-1)
+					first = false
+				end
+			end					
+			msg = msg..']'
+		else
+			msg = msg..'{'
+			for k,v in pairs(obj) do
+				local t = type(v)
+
+				local keyType = type(k)
+				if keyType == 'string'  then
+					if not antiCirc[v] then
+						if not first then 
+							msg = msg .. ","
+						end
+						msg = msg .. "\"".. helms.util.escapeLuaString(k) .."\":" .. helms.util.obj2json(v,antiCirc,maxdepth-1)
+						first = false						
+					end
+				end
+			end					
+			msg = msg..'}'
+		end
+
+	elseif t == 'string' then
+		msg = msg.."\"".. helms.util.escapeLuaString(obj) .."\""
+	elseif t == 'number' then
+		msg = msg..obj
+	elseif t == 'boolean' then
+		if t then
+			msg = msg..'true'
+		else
+			msg = msg..'false'
+		end
+	end
+	return msg
 end
 
 --[[
